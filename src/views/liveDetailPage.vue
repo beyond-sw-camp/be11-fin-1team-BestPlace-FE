8ddff876ecae827f54174d2cb5d6524dd17107f5
ki-hyun-Jang <skjwj0511@gmail.com>
2025-04-17 04:50:44 +0900

    제목 글씨체 변경

--- src/views/liveDetailPage.vue
+++ src/views/liveDetailPage.vue
@@ -587,6 +587,7 @@ onBeforeUnmount(() => {
   font-weight: 600;
   color: #fff;
   margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Apple SD Gothic Neo', 'Malgun Gothic', '맑은 고딕', 'Noto Sans KR', sans-serif;
 }
 
 .stream-meta {
